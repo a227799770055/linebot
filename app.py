ce464213341ae647970a57916af478f12ffe1fbe
eddie <kthsiao1992@gmail.com>
2022-09-25 11:58:51 +0800

    push

--- app.py
+++ app.py
@@ -18,12 +18,15 @@ line_bot_api.push_message(config['LineBot']['username'], TextSendMessage(text='I
 def callback():
     signature = request.headers['X-Line-Signature']
 
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
